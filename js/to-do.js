6080c6b4f9e7313a15db570d7c9a22a1cba02e43
hhsdev <heinhtetsoe.dev@gmail.com>
2020-02-11 02:35:38 +0630

    Minor animation tweaks and refactorings

--- js/to-do.js
+++ js/to-do.js
@@ -1,55 +1,90 @@
-const converter = new showdown.Converter();
-let to_do_list = '## To-do List'
+"use strict";
 
-const add_to_list = () => {
-  $('#text-area').html(converter.makeHtml(to_do_list))
+class ButtonAndTextarea {
+  constructor() {
+    this.create_elements();
+    this.attach_handlers();
+  }
+
+  create_elements() {
+    this.text = '## To-do list';
+    this.listeners = [];
+
+    this.button = $('<button>', { id: 'add-text-btn', class: 'add-text__btn' });
+    this.button.append($('<i>', { class: 'far fa-edit'}));
+    this.textarea = $('<textarea>', { id: 'add-text-textarea', class: 'add-text__textarea'}).val(this.text);
+    this.save = $('<button>', { class: 'to-do__textarea-btn to-do__textarea-btn--save' }).text('save');
+    this.close = $('<button>', { class: 'to-do__textarea-btn to-do__textarea-btn--close' }).text('close');
+  }
+
+  add_to(root) {
+    root.append(this.button).append(this.textarea).append(this.save).append(this.close);
+    this.textarea.css('display', 'none');
+    this.close.css('display', 'none');
+    this.save.css('display', 'none');
+  }
+  attach_handlers() {
+    this.button.on('click', (event) => this.to_textarea(event));
+    this.save.on('click', (event) => this.on_save(event));
+    this.close.on('click', (event) => this.to_button(event));
+  }
+
+  to_textarea(event) {
+    const { button, textarea, save, close } = this;
+    button.css('animation', '0.4s to-textbox');
+    $('i', button).css('animation', '0.4s rotate-out');
+    button.one('animationend', () => {
+      button.css('animation', '');
+      $('i', button).css('animation', '');
+      button.css('display', 'none');
+      textarea.css('display', '');
+      textarea.focus();
+      save.slideDown();
+      close.slideDown();
+    })
+  }
+
+  to_button(event) {
+    const { button, textarea } = this;
+    textarea.css('animation', '0.4s to-button');
+    $('.to-do__textarea-btn').slideUp();
+    textarea.one('animationend', () => {
+      textarea.css('animation', '');
+      textarea.css('display', 'none');
+      button.css('display', '');
+      button.on('click', (event) => this.to_textarea(event));
+    });
+  }
+
+  on_save() {
+    this.text = this.textarea.val();
+    this.emit_save_event();
+    this.textarea.focus();
+  }
+  emit_save_event() {
+    for (let listener of this.listeners) {
+      listener.handle( { type: 'save', data: this.text });
+    }
+  }
 }
 
-let button = $('#add-text')
-let textarea = $('<textarea>', {id: 'add-text-textarea', class: 'to-do__textarea'})
-textarea.val(to_do_list)
-const to_textarea = () => {
-  button.css('animation', '0.4s to-textbox')
-  $('i', button).css('animation', '0.4s rotate-out')
-  button.one('animationend', function() {
-    button.css('animation', '')
-    $('i', button).css('animation', '')
-    button.replaceWith(textarea)
-    spawn_textarea_buttons()
-    textarea.focus()
-  })
+class MarkdownConverter {
+  constructor(target) {
+    this.converter = new showdown.Converter();
+    this.target = target;
+  }
+
+  handle(event) {
+    if (event.type === 'save') {
+      this.target.html(this.converter.makeHtml(event.data));
+    }
+  }
 }
-button.on('click', to_textarea)
+const init_to_do = ($root) => {
+  const converter = new MarkdownConverter($('#text-area'));
 
-const spawn_textarea_buttons = () => {
-  const save = $('<button>', { class: 'to-do__textarea-btn to-do__textarea-btn--save' })
-  const close = $('<button>', { class: 'to-do__textarea-btn to-do__textarea-btn--close' })
-
-  save.on('click', on_save)
-  close.on('click', on_close)
-
-  close.text('close').hide().insertAfter(textarea).slideDown()
-  save.text('save').hide().insertAfter(textarea).slideDown()
+  const todo_editor = new ButtonAndTextarea();
+  todo_editor.add_to($root);
+  todo_editor.listeners.push(converter);
+  todo_editor.emit_save_event();
 }
-
-const animate_button_transition = () => {
-  textarea.css('animation', '0.4s to-button')
-  $('.to-do__textarea-btn').slideUp()
-  textarea.one('animationend', function(event) {
-    textarea.css('animation', '')
-    textarea = textarea.replaceWith(button)
-    button.on('click', to_textarea)
-  })
-}
-
-const on_save = (event) => {
-  to_do_list = textarea.val()
-  add_to_list()
-  textarea.focus()
-}
-
-const on_close = (event) => {
-  animate_button_transition()
-}
-
-add_to_list()
